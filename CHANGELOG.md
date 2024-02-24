b20ce3078eaa86c25ef77dc87c89d93743433f66
kacpergoluchowski <kgoluchowski112@gmail.com>
2024-02-24 14:11:42 +0100

    CHANGELOG.md changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
+## [0.0.2] - 2024-02-24
+
+### Added
+ - Add start page. 
+
+### Fixed
+
+
 ## [0.0.1] - 2024-02-23
  - Initial project. Installed react, electron, vite, sass. checked action of ipcMain (correct).
 
--- ski-jump-manager/src/main.jsx
+++ ski-jump-manager/src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import StartPage from './pages/StartPage.jsx'
 
